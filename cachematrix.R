fad539687cf24971f6ae6eb53b2be0699e74cc77
samhauz <srhauzel@gmail.com>
2014-05-21 21:35:02 +0930

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## By mean of OOP concept encapsulation, the following two functions improve the performance of solve()
-## when it is used for the calcilating the inverse of a matrix.
+## Using OOP concept "encapsulation" the following two functions improve the performance of solve()
+## when it is used for the calculating the inverse of a matrix.
 
 
 ## makeCacheMatrix create matrix object. 
-## This function set and get the value of the matrix and set and get the inverse value of the matrix
+## This function set & get the value of the matrix and set & get the inverse value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) 
 
@@ -20,29 +20,29 @@ makeCacheMatrix <- function(x = matrix())
     setInvMat <-function(inverseMatrix) invmatrix <<-inverseMatrix
     getInvMat <-function() invmatrix
     
-    list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
+    list(set = set, get = get, setInvMat = setInvMat, c = getInvMat)
 }
 
 
 ## cacheSolve function returns the inverse of the matrix that is created by the makeCacheMatrix() above
-## It check if the cache inverse of the matrix is already calcualte. If so, it retrieve the inverse value.
-## if not, it calcualte the inverse of the matrix and set this inverse value  in the cache via setinverse.
+## It check if the inverse of the matrix is already being calcualted. If so, it retrieve the inverse value from the cache.
+## If not, it calcualte the inverse of the matrix and set this inverse value in the cache via setInvMat.
 
 cacheSolve <- function(x, ...) 
 
 {        ## Return a matrix that is the inverse of 'x'
          
-         invmatrix <- x$getinverse()
-            if (!is.null(inv_x)) 
+         invmatrix <- x$getInvMat()
+            if (!is.null(invmatrix)) 
             {
-                message("getting cached inverse matrix")
+                message("Obtaining cached inverse matrix")
                 return(invmatrix)
             } 
             
             else 
             {
                 invmatrix <- solve(x$get())
-                x$setinverse(invmatrix)
+                x$setInvMat(invmatrix)
                 return(invmatrix)
             }
 }
